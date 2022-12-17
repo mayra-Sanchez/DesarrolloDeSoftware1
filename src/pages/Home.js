69ab91d96347b788961ac84b50b1ca01d40b1669
Mayra Alejandra Sanchez Salinas <mayra.alejandra.sanchez@correounivalle.edu.co>
2022-12-17 15:31:05 -0500

    Background principal responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,34 +6,36 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div class="d-flex justify-content-center">
-        <ContentRec
-          title="Bienvenido a la plataforma digital de SIGEIN,
+    <div class="contenedor-inicial">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div class="d-flex justify-content-center">
+          <ContentRec
+            title="Bienvenido a la plataforma digital de SIGEIN,
             en la cual dependiendo de tu rol, podrás realizar diversas acciones "
-          bgcolor="rgba(229, 230, 194, 1)"
-        />
-      </div>
-      <br />
-      <div>
-        <Link to="/SignIn" className="btn btn-outline-dark btn-lg">
-          Iniciar sesión
-        </Link>
-        <Link to="/Admin" className="btn btn-outline-dark btn-lg">
-          Admin
-        </Link>
-        <Link to="/Gerente" className="btn btn-outline-dark btn-lg">
-          Gerente
-        </Link>
-        <Link to="/Operador" className="btn btn-outline-dark btn-lg">
-          Operador
-        </Link>
-        <Link to="/Cliente" className="btn btn-outline-dark btn-lg">
-          Cliente
-        </Link>
+            bgcolor="rgba(229, 230, 194, 1)"
+          />
+        </div>
+        <br />
+        <div>
+          <Link to="/SignIn" className="btn btn-outline-dark btn-lg">
+            Iniciar sesión
+          </Link>
+          <Link to="/Admin" className="btn btn-outline-dark btn-lg">
+            Admin
+          </Link>
+          <Link to="/Gerente" className="btn btn-outline-dark btn-lg">
+            Gerente
+          </Link>
+          <Link to="/Operador" className="btn btn-outline-dark btn-lg">
+            Operador
+          </Link>
+          <Link to="/Cliente" className="btn btn-outline-dark btn-lg">
+            Cliente
+          </Link>
+        </div>
       </div>
     </div>
   );
