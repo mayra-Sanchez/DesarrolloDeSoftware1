a2eec98cf09405a4b83cda5b7964d99f67eb35e6
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2022-12-28 22:30:12 -0500

    arreglo modulos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Gerente from "./gerente/pages/GerenteHome";
 import Operador from "./operador/pages/OperadorHome";
 import Cliente from "./cliente/pages/ClienteHome";
 import RegistrarUsuario from "./admin/pages/RegistrarUsuario";
+import ConsultaCliente from "./operador/pages/ConsultaCliente";
 import { LoginContext } from "./contex/Logincontext";
 
 function App() {
@@ -24,6 +25,10 @@ function App() {
           <Route
             path="Admin/Registrar-Usuarios"
             element={<RegistrarUsuario />}
+          />
+          <Route 
+            path="Operador/Consultar-Cliente" 
+            element={<ConsultaCliente />} 
           />
         </Routes>
       ) : (
