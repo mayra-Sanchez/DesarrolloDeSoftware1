eaf3eaa60ddc50714559cd87996d3281dfe439ae
juanBailon <juanojo2001.5@gmail.com>
2023-01-12 21:46:23 -0500

    adding electricity_price_generator.py to app energy_products

---
+++ SIGEIN/energy_products/electricity_price_generator.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random float between 400.00 and 900.00
+def generate_kwh_price(min=400.00, max=800.00):
+    return random.uniform(min, max)
+
+##print( generate_kwh_price() )
