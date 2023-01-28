ee4dfa6bf7464fdf500319b28fae124916c9f8c8
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2023-01-28 10:48:29 -0500

    inicio tabla consumo cliente

--- src/gerente/pages/ConsumoClientes.js
+++ src/gerente/pages/ConsumoClientes.js
@@ -1,9 +1,51 @@
 import "../hojaestilo/ConsumoCliente.css";
-import React from "react";
+import Axios from "axios";
 import logo from "../Images/logo-2.png";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const ConsumoCliente = () => {
+
+  const [usuarios, setUsuarios] = useState([]);
+  const [tablaUsuarios, setTablaUsuarios] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+
+  const peticion = async () => {
+    await Axios.get("http://127.0.0.1:8000/clients/list-all/")
+      .then((response) => {
+        setUsuarios(response.data);
+        setTablaUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    peticion();
+  }, []);
+
+  const handleChange = (e) => {
+    setBusqueda(e.target.value);
+    filtro(e.target.value);
+  };
+
+  const filtro = (busqueda) => {
+    var resultadosBusqueda = tablaUsuarios.filter((elemento) => {
+      if (
+        elemento.name
+          .toString()
+          .toLowerCase()
+          .includes(busqueda.toLowerCase()) ||
+        elemento.name.toString().toLowerCase().includes(busqueda.toLowerCase())
+      ) {
+        return elemento;
+      }
+    });
+    setUsuarios(resultadosBusqueda);
+  };
 
     return (
     <div class="contenedor-inicialGerente">
@@ -25,13 +67,47 @@ const ConsumoCliente = () => {
         </ul>
       </nav>
 
+      <div className="consultac">
+        <br />
+        <div className="barra-busqueda">
+          <input
+            className="form-control inputBuscar"
+            value={busqueda}
+            placeholder="Búsqueda por identificación"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="table-responsive">
+          <table className="table table-striped table-hover table-responsive-sm">
+            <thead class="thead-dark">
+              <tr>
+                <th>Identificación</th>
+                <th>Mes</th>
+                <th>Kw/h consumidos</th>
+                <th>Precio Kw/h</th>
+                <th>Total a pagar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usuarios &&
+                usuarios.map((usuario) => (
+                  <tr key={usuario.id}>
+                    <td>{usuario.phone}</td>
+                    <td>{usuario.name}</td>
+                    <td>{usuario.username}</td>
+                    <td>{usuario.email}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       
         <div className="boton-home">
           <Link to="/Gerente" className="btn btn-success btn-lg">
             Volver
           </Link>
         </div>
-
+      </div>
     </div>
   );
 };
