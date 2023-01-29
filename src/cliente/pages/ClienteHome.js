4d1fe9b93e6dc3e626d4bb54dba6fd71c54d8807
Paula Giraldo 2022411 <maria.paula.giraldo@correounivalle.edu.co>
2023-01-29 16:08:11 -0500

    inicio dash

--- src/cliente/pages/ClienteHome.js
+++ src/cliente/pages/ClienteHome.js
@@ -1,14 +1,28 @@
-import "../hojaestilo/ClienteHome.css";
-import logo from "../Images/logo-2.png";
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import { Link } from "react-router-dom";
-import { ContenedorCliente } from "../components/ContenedorCliente";
+import "../hojaestilo/ClienteHome.css"
+import logo from "../Images/logo-2.png";
 
-import React from "react";
+const mdTheme = createTheme();
 
-const ClienteHome = () => {
+function DashboardContent() {
+
   return (
-    <div class="contenedor-inicialCliente">
-      <div className="Cliente">
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <a class="navbar-brand">
             <img
@@ -18,7 +32,7 @@ const ClienteHome = () => {
               class="d-inline-block align-top"
               alt="logo"
             />
-            SIGEIN
+            SIGEIN Bienvenido
           </a>
           <ul class="navbar-nav ml-auto">
             <Link to="/SignIn" className="btn btn-light btn-lg">
@@ -26,14 +40,60 @@ const ClienteHome = () => {
             </Link>
           </ul>
         </nav>
-        <ContenedorCliente
-          texto1_Cliente="Bienvenido/a"
-          imagen="cliente"
-          texto2_Cliente="¿Qué deseas hacer?"
-        />
-      </div>
-    </div>
-  );
-};
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Consumo mensual */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height:240}}>
+                </Paper>
+              </Grid>
+              {/* Facturas vencidas */}
+              <Grid item xs={12} md={6} lg={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 120,
+                  }}
+                >
+                </Paper>
+              </Grid>
+              {/* Pagar online */}
+              <Grid item xs={12} md={6} lg={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 120,
+                  }}
+                >
+                </Paper>
+              </Grid>
+              
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
 
-export default ClienteHome;
+  );
+}
+
+export default function ClienteHome() {
+  return <DashboardContent />;
+}
